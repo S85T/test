74b2ca32ef32bacf564b37faa5fc3fcb4fbdaca3
root <root@ucs-7177.test.intranet>
2016-10-10 01:07:51 +0200

    python script ergänzt

--- test.py
+++ test.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python
 import time
+import ConfigParser
 print "Hallo Welt!"
 print (time.strftime("%d.%m.%Y %H:%M:%S"))
+f = open('config.ini','r')
+config = ConfigParser.RawConfigParser()
+#config.add_section('Database')
+#config.set('Database','DB','DefaultDB')
+#config.set('Database','DBUser','DefaultDBUser')
+#config.set('Database','DBPass','DefaultDBPass')
 
+#with open('config.cfg','wb') as configfile:
+# config.write(configfile)
+
+config.read('config.cfg')
+print config.get('Database','db')
+print config.get('Database','dbuser')
+print config.get('Database','dbpass')
